039dfa84ae489b16925ae0f64bd0e4b1c6ebd3f6
andreilaurentiuradu <117320481+andreilaurentiuradu@users.noreply.github.com>
2022-12-02 21:52:28 +0200

    merg B-urile si codA

--- codificari.c
+++ codificari.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #define nmax 1001
+
 void afisare(char s[], int n) {
     for(int i = 0; i < n; ++i) {
         printf("%c", s[i]);
@@ -26,6 +27,7 @@ void codificareB(char s[], int n, int nr, char alfabet[]) {
     
     char p[501];
     strcpy(p, s + n - nr);
+    s[n - nr] = '\0';
     for(int i = 0; i < nr; ++i){
         if(p[i] != ' '){
             if(litera_mica(p[i]))
@@ -34,15 +36,13 @@ void codificareB(char s[], int n, int nr, char alfabet[]) {
                 p[i] = alfabet[p[i] - 'A' + 26 + nr % 52];
         }
     }
-    s[n - nr + 1] = '\0';
     strcat(p, s);//concateneaza pana la \0
     strcpy(s, p);
     s[n] = '\0';
-    puts(s);
+    printf("%s\n", s);
 }
 
 
-//netestat
 void decodificareB(char s[], int n, int nr, char alfabet[]) {
     char p[501], aux[501];
 
@@ -56,13 +56,119 @@ void decodificareB(char s[], int n, int nr, char alfabet[]) {
     }
     //puts(s);
     strncpy(p, s, nr);
+    p[nr] = '\0';
     //puts(p);
     strcpy(aux, s + nr);
+    aux[n - nr] = '\0';
     //puts(aux);
     strcpy(s, aux);
-    s[n] = '\n';
+    s[n] = '\0';
     strcat(s, p);
-    puts(s);
+    printf("%s\n", s);
+}
+
+
+void swap_char(char *x, char *y) {
+    char aux = *x;
+    *x = *y;
+    *y = aux;
+}
+
+void swap_int(int *x, int *y) {
+    int aux = *x;
+    *x = *y;
+    *y = aux;
+}
+
+void swap_coloane(char a[nmax][nmax], int x, int y, int nr_linii) {
+    for(int i = 0; i < nr_linii; i++) {
+        swap_char(&a[i][x], &a[i][y]);
+    }
+}
+void sortare(char a[nmax][nmax], char cheie[], int n, int nr_linii) {
+
+    for(int i = 0; i < n - 1; ++i) {
+        for(int j = i + 1; j < n; ++j) {
+            if(cheie[i] > cheie[j]){
+                swap_char(&cheie[i], &cheie[j]);
+                //swap_int(&indici[i], &indici[j]);
+                swap_coloane(a, i, j, nr_linii);
+            }
+        }
+    }
+}
+
+void creare_matriceA(char a[nmax][nmax], int n, int lungime_cheie, char s[]) {
+    int lin = 0, col = 0;
+
+    for(int i = 0; i < n; ++i) {
+        if(col == lungime_cheie){
+            col = 0;
+            lin++;
+        }
+        a[lin][col] = s[i];
+        col++;
+    }
+}
+
+void afisare_vector(int v[], int n) {
+    for(int i = 0; i < n; ++i) {
+        printf("%d ", v[i]);
+    }
+    printf("\n");
+}
+
+
+///stergem caracterul de pe pozitia poz
+void stergere_caracter(char s[], int n, int poz){
+    for(int i = poz; i < n; ++i)
+        s[i] = s[i + 1];
+    ///pui \n for safety
+}
+void afisare_matrice(char a[nmax][nmax], int n, int m) {
+    for(int i = 0; i < n; ++i)
+     {
+        for(int j = 0; j < m; ++j)
+            printf("%c", a[i][j]);
+        printf("\n");
+     }
+}
+
+void codificareA(char s[], int n, char cheie[], int lungime_cheie) {
+    
+    char a[nmax][nmax];
+    for(int i = 0; i < 500; ++i) {
+        for(int j = 0; j < 500; ++j) {
+            a[i][j] = '0';
+        }
+    }
+
+    int nr_linii;
+    nr_linii = n / lungime_cheie;
+    if(n % lungime_cheie != 0)
+        nr_linii++;
+
+    //cream matricea
+    creare_matriceA(a, n, lungime_cheie, s);
+    sortare(a, cheie, lungime_cheie, n);
+    int k = 0;
+    for(int j = 0; j < lungime_cheie; ++j){
+        for(int i = 0; i < nr_linii; ++i) {
+            if(a[i][j] != '0'){
+                //printf("%c", a[i][j]);
+                s[k] = a[i][j];
+            }
+            else
+            {
+                //printf(" ");
+                s[k] = ' ';
+            }
+            ++k;
+        }
+        //printf("\n");
+    }
+    s[k] = '\0';
+    printf("%s\n", s);
 }
 
 int main() {
@@ -98,15 +204,22 @@ int main() {
         }
 
         if(cod[0] == 'C' && cod[strlen(cod) - 1] == 'A') {
-            getchar();
+            getchar();//scap de spatiu de dupa codificare
             //printf("CHEIA ESTE :");
             fgets(cheie, 51, stdin);
             //puts(cheie);
             //printf("before\n");
             //puts(s);
-            codificareA(s, n, cheie);
+            int lungime_cheie = strlen(cheie);
+            //printf("lungime cheie = %d", lungime_cheie);
+            lungime_cheie--;
+            cheie[lungime_cheie] = '\0';
+            codificareA(s, n, cheie, lungime_cheie);
             //printf("after\n");
         }
+
+        //actualizam dimensiunea sirului
+        n = strlen(s);
         
     }while(1);
 
