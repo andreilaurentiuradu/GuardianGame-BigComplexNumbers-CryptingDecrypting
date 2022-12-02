40b51329a77ddb04272b20f4c6b918c081fd7d68
andreilaurentiuradu <117320481+andreilaurentiuradu@users.noreply.github.com>
2022-12-02 19:47:46 +0200

    mergB-urile

--- codificari.c
+++ codificari.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <stdbool.h>
-
+#define nmax 1001
 void afisare(char s[], int n) {
     for(int i = 0; i < n; ++i) {
         printf("%c", s[i]);
     }
 }
+
 bool litera_mare(char ch) {
     if('A' <= ch && ch <= 'Z')
         return true;
@@ -22,9 +24,6 @@ void codificareB(char s[], int n, int nr, char alfabet[]) {
     //n e lungimea sirului
     //n - 1 - nr e pozitia ultimului element de pastrat in s
     
-    // s[n - 1 - nr] = '\0';
-    // //char ans = 
-    // puts(p);
     char p[501];
     strcpy(p, s + n - nr);
     for(int i = 0; i < nr; ++i){
@@ -36,32 +35,79 @@ void codificareB(char s[], int n, int nr, char alfabet[]) {
         }
     }
     s[n - nr + 1] = '\0';
-    strcat(p, s);
+    strcat(p, s);//concateneaza pana la \0
     strcpy(s, p);
     s[n] = '\0';
     puts(s);
 }
+
+
+//netestat
+void decodificareB(char s[], int n, int nr, char alfabet[]) {
+    char p[501], aux[501];
+
+    for(int i = 0; i < nr; ++i) {
+        if(s[i] != ' '){
+            if(litera_mica(s[i]))
+                s[i] = alfabet[s[i] - 'a'  + 52 - nr % 52];
+            else
+                s[i] = alfabet[s[i] - 'A'  + 78 - nr % 52];
+        }
+    }
+    //puts(s);
+    strncpy(p, s, nr);
+    //puts(p);
+    strcpy(aux, s + nr);
+    //puts(aux);
+    strcpy(s, aux);
+    s[n] = '\n';
+    strcat(s, p);
+    puts(s);
+}
+
 int main() {
     int n, nr;
-    char s[501], cod[501];
-    char alfabet[100] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char s[581], cod[581], cheie[51];
+    char alfabet[105] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+    //s e sirul de decodificat
     fgets(s, 501, stdin);
     n = strlen(s);
-     --n;
+    --n;
     s[n] = '\0';
     do
     {
         scanf("%s", cod);
+
+        // daca am ajuns la STOP
         if(strcmp(cod, "STOP") == 0)
             return 0;
-        scanf("%d", &nr);
-        getchar(); // ala de dupa nr
-        //printf("%d\n", n);
-        //puts(s);
-        //puts(cod);
-        //printf("%d\n\n", nr);
-        codificareB(s, n, nr, alfabet);
+        
+        // daca codificarea e de tip B
+        if(cod[0] == 'C' && cod[strlen(cod) - 1] == 'B'){
+            scanf("%d", &nr);
+            getchar(); // ala de dupa nr
+            codificareB(s, n, nr, alfabet);
+        }
+
+        //daca decodificarea e de tip B
+        if(cod[0] == 'D' && cod[strlen(cod) - 1] == 'B') {
+            scanf("%d", &nr);
+            getchar(); // ala de dupa nr
+            decodificareB(s, n, nr, alfabet);
+        }
+
+        if(cod[0] == 'C' && cod[strlen(cod) - 1] == 'A') {
+            getchar();
+            //printf("CHEIA ESTE :");
+            fgets(cheie, 51, stdin);
+            //puts(cheie);
+            //printf("before\n");
+            //puts(s);
+            codificareA(s, n, cheie);
+            //printf("after\n");
+        }
+        
     }while(1);
 
     printf("\n");
