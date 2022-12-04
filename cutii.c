18e5e8e8432c9f07231924aa306c71f943fb41ef
andreilaurentiuradu <117320481+andreilaurentiuradu@users.noreply.github.com>
2022-12-04 14:31:07 +0200

    cutii cu comentarii

--- cutii.c
+++ cutii.c
@@ -2,65 +2,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-bool testare_nr_cutii(int start, int n, int p[]) {
-    int nod = start, ans = 0;
+//functie pentru verificarea unui prizonier
+bool testare_nr_cutii(int start, int p, int cutii[]) {
+    //pornim de la prima cutie si retinem numarul de cutii deschise pana ajungem la cutia buna
+    int nod = start, deschise = 0;
+
     do {
-        ans++;
-        nod = p[nod];
+        deschise++;
+        nod = cutii[nod];
     } while (nod != start);
-    return (ans <= n / 2);
+
+    //intoarcem valoarea de adevar a propozitiei:"prizonierul a deschis cel mult p/2 cutii"
+    return (deschise <= p / 2);
 }
 
-void afisare(int start, int p[]) {
+//functie pentru afisarea ciclurilor
+void ciclu(int start, int p, int cutii[], int viz[]) {
     int nod = start;
-    printf("%d", nod);
-    nod = p[nod];
-    while (nod != start){
-        printf(" %d", nod);
-        nod = p[nod];
-    }
-    printf("\n");
-}
 
-void ciclu(int start, int n, int p[], int viz[]) {
-    int nod = start, ans = 0;
-    // neviz = 0;
-    if (viz[p[start]] == false) {
-        do {
+    //verificam daca am gasit un ciclu nou( cutia de la care pornim nu poate sa faca parte din 2 cicluri)
+    if (viz[cutii[start]] == false) {
+        //marcam cutia ca facand parte dintr-un ciclu
+        viz[nod] = true;
+
+        //afisam prima cutie pentru a nu avea probleme cu spatiul de dupa ultima cutie a ciclului
+        printf("%d", nod);
+
+        //pornim de la a doua cutie si parcurgem ciclul
+        nod = cutii[nod];
+        while (nod != start){
+            //afisam un spatiu si cutia curenta
+            printf(" %d", nod); 
+            //trecem la urmatoarea cutie
+            nod = cutii[nod];
+            //marcam cutia ca facand parte dintr-un ciclu
             viz[nod] = true;
-            nod = p[nod];
-            ans++;
-        } while (nod != start);
-
-        // if(neviz == ans)
-        afisare(start, p);
+        }
+        printf("\n");
     }
-    // return ans;
 }
 
 int main() {
-    int n, p[505], viz[505] = {0};
-    scanf("%d", &n);
-    for (int i = 1; i <= n; ++i) {
-        scanf("%d", &p[i]);
+    int p, cutii[501], viz[501] = {0};
+
+    //citim numarul de prizonieri
+    scanf("%d", &p);
+
+    //citim pentru fiecare prizonier cutia pe care o deschide prima data
+    for (int i = 1; i <= p; ++i) {
+        scanf("%d", &cutii[i]);
     }
-    int ok = 1;
-    for (int i = 1; i <= n; ++i) {
-        if (testare_nr_cutii(i, n, p) == false) {
-            ok = 0;
+
+    //luam o variabila booleana in care vom retine daca prizonierii au castigat sau nu jocul plecand de la presupunerea ca il vor castiga
+    bool ok = true;
+
+    //verificam pentru fiecare prizonier daca deschide mai mult de p/2 cutii
+    for (int i = 1; i <= p; ++i) {
+        //daca a deschis mai mult de p/2 cutii functia va intoarce false
+        if (testare_nr_cutii(i, p, cutii) == false) {
+            //am gasit un prizonier care deschide mai mult de p/2 cutii asa ca actualizam variabila booleana, jocul fiind pierdut
+            ok = false;
+            //iesim din structura repetitiva deoarece am aflat deznodamantul jocului si nu mai trebuie sa ii verificam si pe ceilalti prizonieri
             break;
         }
     }
 
-    if (ok == 1)
+    //afisam deznodamantul jocului
+    if (ok)
         printf("Da\n");
     else
         printf("Nu\n");
 
-    for (int i = 1; i <= n; ++i) {
-        // printf("%d\n", ciclu(i));
-        ciclu(i, n, p, viz);
-        // afisare(i);
+    //afisam ciclurile formate de cutii
+    for (int i = 1; i <= p; ++i) {
+        ciclu(i, p, cutii, viz);
     }
 
     return 0;
