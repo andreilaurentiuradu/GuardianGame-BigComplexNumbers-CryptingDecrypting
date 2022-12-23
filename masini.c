9d62fb6c72ba4d01e3bd0aaea915ea323da12bd2
andreilaurentiuradu <117320481+andreilaurentiuradu@users.noreply.github.com>
2022-12-23 02:15:32 +0200

    +comentarii

--- masini.c
+++ masini.c
@@ -1,102 +1,126 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
-#define l_max 20
+#define l_max 20  // lungimea maxima a fiecarui sir de caractere citit
 #define pret_benzina 8.02
 #define pret_motorina 9.29
+#define nr_tipuri 4
 
-void cerinta_unu(char **combustibil, int n, int frecv[4]) {
-    for(int i = 0; i < n; ++i) {
-        if(strcmp(combustibil[i], "benzina") == 0)
-            frecv[0]++;
-        if(strcmp(combustibil[i], "motorina") == 0)
-            frecv[1]++;
-        if(strcmp(combustibil[i], "hibrid") == 0)
-            frecv[2]++;
-        if(strcmp(combustibil[i], "electric") == 0)
-            frecv[3]++;
+// retinem intr-un vector de frecventa numarul de masini care functioneaza cu
+// fiecare tip de combustibil
+void cerinta_unu(char **combustibil, int n, int frecv[]) {
+    for (int i = 0; i < n; ++i) {
+        if (strcmp(combustibil[i], "benzina") == 0) frecv[0]++;
+        if (strcmp(combustibil[i], "motorina") == 0) frecv[1]++;
+        if (strcmp(combustibil[i], "hibrid") == 0) frecv[2]++;
+        if (strcmp(combustibil[i], "electric") == 0) frecv[3]++;
     }
 }
 
-float consum_per_masina(double consum, int km) {
-    return consum * km / 100;
-}
+// consumul unei masini
+double consum_per_masina(double consum, int km) { return consum * km / 100; }
 
-void grupez_branduri(char **brand, float *litri, double *lei,  int n, bool *viz){
-    float litri_total;
+// determinam prima aparitie a unui brand nou si retinem in acea pozitie
+// consumul total al brandului si suma totala
+void grupez_branduri(char **brand, double *litri, double *lei, int n,
+                     bool *viz) {
+    double litri_total;
     double lei_total;
-    for(int i = 0; i < n; ++i) {
-        if(!viz[i]){
+    for (int i = 0; i < n; ++i) {
+        // daca am gasit un brand nou
+        if (!viz[i]) {
             litri_total = litri[i];
             lei_total = lei[i];
-            for(int j = i + 1; j < n; ++j) {
-                if(strcmp(brand[i], brand[j]) == 0){
-                    //daca sunt acelasi brand
+
+            // cautam celelate masini din acelasi brand
+            for (int j = i + 1; j < n; ++j) {
+                if (strcmp(brand[i], brand[j]) == 0) {
+                    // daca sunt acelasi brand actualizam consumul si costul
+                    // total si marcam masina
                     litri_total += litri[j];
                     lei_total += lei[j];
                     viz[j] = true;
                 }
             }
-            //printf("litri : %f      lei : %lf\n", litri_total, lei_total);
+
+            // actualizam in vectori in prima aparitie a brandului
             litri[i] = litri_total;
             lei[i] = lei_total;
         }
     }
 }
 
-//urmeaza sa le separi in functie de brand parcurgand vectorul de branduri si marcand brandurile folosite
-void cerinta_doi(char **brand, char **combustibil, double *consum, int n, int *km, float *litri, double *lei, bool *viz) {
-    //cream pentru fiecare masina consumul_total si pretul
-    for(int i = 0; i < n; ++i) {
+void cerinta_doi(char **brand, char **combustibil, double *consum, int n,
+                 int *km, double *litri, double *lei, bool *viz) {
+    // retinem pentru fiecare masina consumul_total si pretul
+    for (int i = 0; i < n; ++i) {
         litri[i] = consum_per_masina(consum[i], km[i]);
-        //printf("i = %d => %f ", i, litri[i]);
-        if(strcmp(combustibil[i], "motorina") == 0)
-            lei[i] = (double) pret_motorina * litri[i];
+
+        // pretul variaza in functie de tipul de combustibil
+        if (strcmp(combustibil[i], "motorina") == 0)
+            lei[i] = (double)pret_motorina * litri[i];
         else
-            lei[i] = (double) pret_benzina * litri[i];
+            lei[i] = (double)pret_benzina * litri[i];
     }
+
     grupez_branduri(brand, litri, lei, n, viz);
 }
 
+// daca este litera mare
+bool litera_mare(char ch) { return ('A' <= ch && ch <= 'Z'); }
 
-bool litera_mare(char ch){
-    return ('A' <= ch && ch <='Z');
-}
+// daca este cifra
+bool cifra(char ch) { return ('0' <= ch && ch <= '9'); }
 
-bool cifra(char ch){
-    return ('0' <= ch && ch <= '9');
-}
-
+// daca numarul unei masini este valid
 bool numar_corect(char **numar, int i) {
+    // retinem numarul de caractere al numarului
     int lungime_numar = strlen(numar[i]);
-    if(lungime_numar < 6 && lungime_numar > 8)
-        return false;
-    
-    //A11AAA
-    if(lungime_numar == 6 && litera_mare(numar[i][0]) && cifra(numar[i][1]) && cifra(numar[i][2]) && litera_mare(numar[i][3]) && litera_mare(numar[i][4]) && litera_mare(numar[i][5]))
-        return true;
-    
-    //A111AAA
-    if(lungime_numar == 7 && litera_mare(numar[i][0]) && cifra(numar[i][1]) && cifra(numar[i][2]) && cifra(numar[i][3]) && litera_mare(numar[i][4]) && litera_mare(numar[i][5]) && litera_mare(numar[i][6]))
+
+    // daca nu are 6, 7 sau 8 cifre numarul va fi sigur invalid
+    if (lungime_numar < 6 && lungime_numar > 8) return false;
+
+    // avem 3 tipuri de numar valid
+    // (A reprezinta caracter si 1 reprezinta cifra)
+
+    // A11AAA
+    if (lungime_numar == 6 && litera_mare(numar[i][0]) && cifra(numar[i][1]) &&
+        cifra(numar[i][2]) && litera_mare(numar[i][3]) &&
+        litera_mare(numar[i][4]) && litera_mare(numar[i][5]))
         return true;
 
-    //AA11AAA
-    if(lungime_numar == 7 && litera_mare(numar[i][0]) && litera_mare(numar[i][1]) && cifra(numar[i][2]) && cifra(numar[i][3]) && litera_mare(numar[i][4]) && litera_mare(numar[i][5]) && litera_mare(numar[i][6]))
-        return true;
-    
-    //AA111AAA
-    if(lungime_numar == 8 && litera_mare(numar[i][0]) && litera_mare(numar[i][1]) && cifra(numar[i][2]) && cifra(numar[i][3]) && cifra(numar[i][4]) && litera_mare(numar[i][5]) && litera_mare(numar[i][6]) && litera_mare(numar[i][7]))
+    // A111AAA
+    if (lungime_numar == 7 && litera_mare(numar[i][0]) && cifra(numar[i][1]) &&
+        cifra(numar[i][2]) && cifra(numar[i][3]) && litera_mare(numar[i][4]) &&
+        litera_mare(numar[i][5]) && litera_mare(numar[i][6]))
         return true;
 
+    // AA11AAA
+    if (lungime_numar == 7 && litera_mare(numar[i][0]) &&
+        litera_mare(numar[i][1]) && cifra(numar[i][2]) && cifra(numar[i][3]) &&
+        litera_mare(numar[i][4]) && litera_mare(numar[i][5]) &&
+        litera_mare(numar[i][6]))
+        return true;
+
+    // AA111AAA
+    if (lungime_numar == 8 && litera_mare(numar[i][0]) &&
+        litera_mare(numar[i][1]) && cifra(numar[i][2]) && cifra(numar[i][3]) &&
+        cifra(numar[i][4]) && litera_mare(numar[i][5]) &&
+        litera_mare(numar[i][6]) && litera_mare(numar[i][7]))
+        return true;
+
+    // daca nu se incadreaza in niciun tip atunci numarul este invalid
     return false;
 }
+
+// retinem in valid daca numarul de inmatriculare al ficarei masini este valid
 void cerinta_trei(char **numar, int n, bool valid[]) {
-    
-    for(int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i) {
         valid[i] = numar_corect(numar, i);
     }
 }
+
 int main() {
     char **brand;
     char **numar;
@@ -105,73 +129,48 @@ int main() {
     int *km;
     int n;
     char cerinta;
+    // citim numarul de masini
     scanf("%d", &n);
 
+    // cream matrici alocate dinamic in care vom retine brandurile, numerele de
+    // inmatriculare si combustibilul
     brand = malloc(n * sizeof(char *));
-    for(int i = 0; i < n; ++i) 
-        brand[i] = calloc(l_max, sizeof(char));
-
-    // scanf("%s", brand[0]);
-    // printf("%s\n", brand[0]);
-
     numar = malloc(n * sizeof(char *));
-    for(int i = 0; i < n; ++i) 
-        numar[i] = calloc(l_max, sizeof(char));
-
     combustibil = malloc(n * sizeof(char *));
-    for(int i = 0; i < n; ++i)
-        combustibil[i] = calloc(l_max, sizeof(char));
-    
+
+    // consumul si km ii retinem in vectori alocati dinamic
     consum = malloc(n * sizeof(double));
     km = malloc(n * sizeof(int));
 
-    bool merge = 0;
-    for(int i = 0; i < n; ++i){
+    // retinem datele fiecarei masini
+    for (int i = 0; i < n; ++i) {
+        brand[i] = calloc(l_max, sizeof(char));
         scanf("%s", brand[i]);
-        merge = realloc(brand[i], strlen(brand[i]) * sizeof(char));
-        // if(merge)
-        //     printf("da\n");
+        brand[i] = (char *)realloc(brand[i], (strlen(brand[i]) + 1) * sizeof(char));
+
+        numar[i] = calloc(l_max, sizeof(char));
         scanf("%s", numar[i]);
-        merge = realloc(numar[i], strlen(numar[i]) * sizeof(char));
-        // if(merge)
-        //     printf("da\n");
+        numar[i] = (char *)realloc(numar[i], (strlen(numar[i]) + 1) * sizeof(char));
+
+        combustibil[i] = calloc(l_max, sizeof(char));
         scanf("%s", combustibil[i]);
-        merge = realloc(combustibil[i], strlen(combustibil[i]) * sizeof(char));
-        // if(merge)
-        //     printf("da\n");
+
+        combustibil[i] = (char *)realloc(combustibil[i],
+                        (strlen(combustibil[i]) + 1) * sizeof(char));
+
         scanf("%lf", &consum[i]);
         scanf("%d", &km[i]);
     }
-    if(!merge)
-        printf("ceva buseste\n");
 
-    getchar();//pt enter
+    // scapam de enter
+    getchar();
+
     scanf("%c", &cerinta);
 
-    /*for(int i = 0; i < n; ++i)
-        printf("%s ", brand[i]);
-    printf("\n");
-
-    for(int i = 0; i < n; ++i)
-        printf("%s ", numar[i]);
-    printf("\n");
-
-    for(int i = 0; i < n; ++i)
-        printf("%s", combustibil[i]);
-    printf("\n");
-
-    for(int i = 0; i < n; ++i)
-        printf("%lf ", consum[i]);
-    printf("\n");
-
-    for(int i = 0; i < n; ++i)
-        printf("%d ", km[i]);
-    printf("\n");*/
-    
-
-    if(cerinta == 'a'){
-        //avem 4 tipuri de combustibil
-        int frecv[4] = {0};
+    // pentru cerinta a
+    if (cerinta == 'a') {
+        // avem 4 tipuri de combustibil
+        int frecv[nr_tipuri] = {0};
         cerinta_unu(combustibil, n, frecv);
         printf("benzina - %d\n", frecv[0]);
         printf("motorina - %d\n", frecv[1]);
@@ -179,44 +178,65 @@ int main() {
         printf("electric - %d\n", frecv[3]);
     }
 
-    if(cerinta == 'b'){
-        float *litri = calloc(n, sizeof(float));
+    if (cerinta == 'b') {
+        // vom retine in 3 vectori alocati dinamic
+        // 3 tipuri de informatii despre masini
+
+        // numarul de litrii consumati de fiecare masina
+        double *litri = calloc(n, sizeof(double));
+        // pretul platit de fiecare masina pentru km parcursi
         double *lei = calloc(n, sizeof(double));
+        // daca masina a fost luata in calcul pentru realizarea costului total
+        // si al combustibilului total consumat
         bool *viz = calloc(n, sizeof(bool));
+
         cerinta_doi(brand, combustibil, consum, n, km, litri, lei, viz);
-        for(int i = 0; i < n; ++i) {
-            if(!viz[i]){
-                printf("%s a consumat %.2f - %.2lf lei\n", brand[i], litri[i], lei[i]);
+
+        // in vectorul viz va ramane nemarcata prima aparitie a fiecarui brand
+        for (int i = 0; i < n; ++i) {
+            if (!viz[i]) {
+                printf("%s a consumat %.2lf - %.2lf lei\n", brand[i], litri[i],
+                       lei[i]);
             }
         }
+
+        // eliberam memoria
         free(litri);
         free(lei);
         free(viz);
     }
-    if(cerinta == 'c'){
+
+    if (cerinta == 'c') {
+        // retinem intr-un vector alocat dinamic daca numarul de inmatriculare
+        // al fiecarei masini este valid
         bool *valid;
         valid = calloc(n, sizeof(bool));
-        cerinta_trei(numar, n, valid);
-        // for(int i = 0; i < n; ++i)
-        //     printf("%d ", valid[i]);
-        // printf("\n");
 
+        cerinta_trei(numar, n, valid);
+
+        // retinem intr-o variabila booleana daca toate masinile au numere de
+        // inmatriculare valide
         bool ok = true;
-        for(int i = 0; i < n; ++i) {
-            if(!valid[i]){
-                printf("%s cu numarul %s: numar invalid\n", brand[i], numar[i]);
+        for (int i = 0; i < n; ++i)
+            // verificam daca masina are numarul de inmatriculare valid
+            if (!valid[i]) {
+                // daca nu, afisam brandul, numarul si mesajul numar invalid
+                printf("%s cu numarul %s", brand[i], numar[i]);
+                printf(": numar invalid\n");
+                // gasind o masina cu numarul invalid inseamna ca nu toate
+                // masinile au numarul valid
                 ok = false;
             }
-        }
 
-        if(ok)
-            printf("Numere corecte!\n");
-        
+        // daca toate masinile au numarul de inmatriculare valid
+        if (ok) printf("Numere corecte!\n");
+
+        // eliberam memoria
         free(valid);
     }
-    
-    //eliberam memoria 
-    for(int i = 0; i < n; ++i){
+
+    // eliberam memoria
+    for (int i = 0; i < n; ++i) {
         free(brand[i]);
         free(numar[i]);
         free(combustibil[i]);
